66c2da6766c6a8699e1b7cf06e037772ac95dacc
Jayamal <jayamaljayamaha2@gmail.com>
2024-02-23 13:08:56 +0530

    feat: add swagger - add swagger open api doc - configure route for swagger open api doc - created required endpoints for speaking

--- src/Index.ts
+++ src/Index.ts
@@ -3,14 +3,25 @@ import cors from "cors";
 import configs from "./config/configs";
 import healthCheckRouter from "./routes/HealthCheckRoute";
 import { SimpleErrorHandler } from "./middlewares/GlobalErrorHandlingMiddleware";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
 
 const app: Express = express();
 
+// cors and json middlewares
 app.use(cors());
 app.use(express.json());
 
+// swagger openapi doc route
+const swaggerDocument = YAML.load("./src/apiDocs/openapi.yaml");
+app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Buisness logic routes
 app.use("/healthcheck", healthCheckRouter);
 
+/* Global middlewares
+ * 1- Error handling middleware
+ */
 app.use(SimpleErrorHandler);
 
 app.listen(process.env.PORT, () => {
