2c4d7533b44bc41c2d724e241e33fbfebd4183c7
Jayamal <jayamaljayamaha2@gmail.com>
2024-02-23 10:24:16 +0530

    feat: error handling - add global error handling middleware - add error classes - configure to throw unauthorized errors for invalid token

--- src/Index.ts
+++ src/Index.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import cors from "cors";
 import configs from "./config/configs";
 import healthCheckRouter from "./routes/HealthCheckRoute";
+import { SimpleErrorHandler } from "./middlewares/GlobalErrorHandlingMiddleware";
 
 const app: Express = express();
 
@@ -9,6 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/healthcheck", healthCheckRouter);
+
+app.use(SimpleErrorHandler);
 
 app.listen(process.env.PORT, () => {
   console.log("Server Listening on PORT:", configs.port);
---
+++ src/middlewares/GlobalErrorHandlingMiddleware.ts
@@ -0,0 +1,9 @@
+import { Response, NextFunction, Request } from "express";
+import ELCIELTSInternalError from "../exception/ELCIELTSInternalError";
+
+export const SimpleErrorHandler = (error: ELCIELTSInternalError, req: Request, res: Response, next: NextFunction) => {
+  res.status(error.getStatus()).json({
+    status: error.getStatus(),
+    message: error.message,
+  });
+};
