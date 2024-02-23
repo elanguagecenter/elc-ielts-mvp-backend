03efefd73f64201700b95cd53f57a8826fb5227c
Jayamal <jayamaljayamaha2@gmail.com>
2024-02-23 09:36:41 +0530

    feat: add token auth (#2)
    - add authorizer interface
    - add cognito authorizer implementation
    - add cognito authorizer middleware
    - create healthcheck middleware

--- src/config/configs.ts
+++ src/config/configs.ts
@@ -1,6 +1,7 @@
 const configs = {
   port: process.env.PORT,
   dbUrl: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_SCHEMA}?schema=${process.env.DB_PORT}`,
+  awsCognitoJwk: process.env.AWS_COGNITO_JWK || "{}",
 };
 
 export default configs;
