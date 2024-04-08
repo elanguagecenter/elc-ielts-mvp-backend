869169da1f17656e635d3af0a57d0a59cf336ee5
Jayamal <jayamaljayamaha2@gmail.com>
2024-04-08 23:40:54 +0530

    feat: add new functions to chatgpt text generator

--- src/services/testGen/ITextGeneratorService.ts
+++ src/services/testGen/ITextGeneratorService.ts
@@ -1,9 +1,18 @@
 interface ITextGeneratorService {
-  generateSpeakingTestStage2(): Promise<string>;
-  generateSpeakingTestStage3(previousGeneratedText: string): Promise<string>;
-  generateWritingTestStage1(): Promise<string>;
-  generateWritingTestStage2(): Promise<string>;
-  evaluateWritingTestStage(question: string, answer: string, testStage: number): Promise<string>;
+  // speaking test
+  generateSpeakingTestStage2(): Promise<Array<string | null>>;
+  generateSpeakingTestStage3(previousGeneratedText: string): Promise<Array<string | null>>;
+
+  // writing test
+  generateWritingTestStage1(): Promise<Array<string | null>>;
+  generateWritingTestStage2(): Promise<Array<string | null>>;
+  evaluateWritingTestStage(question: string, answer: string, testStage: number): Promise<Array<string | null>>;
+
+  // reading test
+  generateReadingTestStageOneText(): Promise<Array<string | null>>;
+  generateReadingTestStageOneMcqQuestions(text: string, numberOfQuestion: number): Promise<Array<string | null>>;
+  generateReadingTestStageOneSentanceCompletionQuestions(text: string, numberOfQuestion: number): Promise<Array<string | null>>;
+  evaluateReadingTestQuestions(text: string, question: string, answer: string): Promise<Array<string | null>>;
 }
 
 export default ITextGeneratorService;
