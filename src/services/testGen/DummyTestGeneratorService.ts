e04c286d709b70d985c2edbdbddc8d81c1d3fe92
Jayamal <jayamaljayamaha2@gmail.com>
2024-04-11 11:22:27 +0530

    fix: resolve typescipt issue

--- src/services/testGen/DummyTestGeneratorService.ts
+++ src/services/testGen/DummyTestGeneratorService.ts
@@ -3,28 +3,42 @@ import ITextGeneratorService from "./ITextGeneratorService";
 class DummyTestGeneratorService implements ITextGeneratorService {
   private static instance: ITextGeneratorService = new DummyTestGeneratorService();
   private constructor() {}
+  generateSpeakingTestStage2(): Promise<(string | null)[]> {
+    throw new Error("Method not implemented.");
+  }
+  generateSpeakingTestStage3(previousGeneratedText: string): Promise<(string | null)[]> {
+    throw new Error("Method not implemented.");
+  }
+  generateWritingTestStage1(): Promise<(string | null)[]> {
+    throw new Error("Method not implemented.");
+  }
+  generateWritingTestStage2(): Promise<(string | null)[]> {
+    throw new Error("Method not implemented.");
+  }
+  evaluateWritingTestStage(question: string, answer: string, testStage: number): Promise<(string | null)[]> {
+    throw new Error("Method not implemented.");
+  }
+  generateReadingTestStageOneText(): Promise<(string | null)[]> {
+    throw new Error("Method not implemented.");
+  }
+  generateReadingTestStageTwoText(): Promise<(string | null)[]> {
+    throw new Error("Method not implemented.");
+  }
+  generateReadingTestStageThreeText(): Promise<(string | null)[]> {
+    throw new Error("Method not implemented.");
+  }
+  generateReadingTestStageMcqQuestions(text: string, numberOfQuestion: number, taskNum: number): Promise<(string | null)[]> {
+    throw new Error("Method not implemented.");
+  }
+  generateReadingTestStageSentanceCompletionQuestions(text: string, numberOfQuestion: number, taskNum: number): Promise<(string | null)[]> {
+    throw new Error("Method not implemented.");
+  }
+  evaluateReadingTestQuestions(text: string, question: string, answer: string): Promise<(string | null)[]> {
+    throw new Error("Method not implemented.");
+  }
 
   static getInstance(): ITextGeneratorService {
     return this.instance;
-  }
-
-  async generateSpeakingTestStage2(): Promise<string> {
-    return Promise.resolve("Explain a memorable vacation you spent");
-  }
-
-  async generateSpeakingTestStage3(previousGeneratedText: string): Promise<string> {
-    return Promise.resolve("What are the things to consider when selecting a place to stay on a vacation");
-  }
-
-  generateWritingTestStage1(): Promise<string> {
-    throw new Error("Method not implemented.");
-  }
-  generateWritingTestStage2(): Promise<string> {
-    throw new Error("Method not implemented.");
-  }
-
-  evaluateWritingTestStage(question: string, answer: string, testStage: number): Promise<string> {
-    throw new Error("Method not implemented.");
   }
 }
 
