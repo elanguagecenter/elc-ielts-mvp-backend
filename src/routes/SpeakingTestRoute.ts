ad453cca27c25f05adad0bf75eeedf100c570287
Jayamal <jayamaljayamaha2@gmail.com>
2024-04-28 10:01:52 +0530

    feat: protect the routes for user type access

--- src/routes/SpeakingTestRoute.ts
+++ src/routes/SpeakingTestRoute.ts
@@ -1,16 +1,37 @@
 import express from "express";
 import { CognitoAuthMiddleware } from "../middlewares/AuthMiddleware";
 import SpeakingTestController from "../controllers/SpeakingTestController";
+import EndPointAccessVerifyMiddleware from "../middlewares/EndPointAccessVerifyMiddleware";
 
 const speakingTestRoute = express.Router({ mergeParams: true });
 const speakingTestController: SpeakingTestController = new SpeakingTestController();
 
 // routes begining /ielts/test/:testId/speaking
-speakingTestRoute.post("/", CognitoAuthMiddleware, speakingTestController.createNewSpeakingTest.bind(speakingTestController));
-speakingTestRoute.get("/", CognitoAuthMiddleware, speakingTestController.getAllSpeakingTestsByTestIdOrUserId.bind(speakingTestController));
-speakingTestRoute.get("/:speakingTestId/stages", CognitoAuthMiddleware, speakingTestController.getNextAvailableWritingTestStages.bind(speakingTestController));
-speakingTestRoute.get("/:speakingTestId/stages/:stageId", CognitoAuthMiddleware, speakingTestController.getSpecificExistingSpeakingStage.bind(speakingTestController));
+speakingTestRoute.post("/", CognitoAuthMiddleware, EndPointAccessVerifyMiddleware.StudentAccess, speakingTestController.createNewSpeakingTest.bind(speakingTestController));
+speakingTestRoute.get(
+  "/",
+  CognitoAuthMiddleware,
+  EndPointAccessVerifyMiddleware.StudentAccess,
+  speakingTestController.getAllSpeakingTestsByTestIdOrUserId.bind(speakingTestController)
+);
+speakingTestRoute.get(
+  "/:speakingTestId/stages",
+  CognitoAuthMiddleware,
+  EndPointAccessVerifyMiddleware.StudentAccess,
+  speakingTestController.getNextAvailableWritingTestStages.bind(speakingTestController)
+);
+speakingTestRoute.get(
+  "/:speakingTestId/stages/:stageId",
+  CognitoAuthMiddleware,
+  EndPointAccessVerifyMiddleware.StudentAccess,
+  speakingTestController.getSpecificExistingSpeakingStage.bind(speakingTestController)
+);
 
-speakingTestRoute.put("/:speakingTestId/stages/:stageId", CognitoAuthMiddleware, speakingTestController.updateSpeakingTestStage.bind(speakingTestController));
+speakingTestRoute.put(
+  "/:speakingTestId/stages/:stageId",
+  CognitoAuthMiddleware,
+  EndPointAccessVerifyMiddleware.StudentAccess,
+  speakingTestController.updateSpeakingTestStage.bind(speakingTestController)
+);
 
 export default speakingTestRoute;
