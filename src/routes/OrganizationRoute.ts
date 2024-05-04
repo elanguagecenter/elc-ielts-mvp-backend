b57ae0ca57215b54aba1d7ea165d31af920f8cef
Jayamal <jayamaljayamaha2@gmail.com>
2024-05-04 22:47:19 +0530

    feat: add required user and organizaiton routes

---
+++ src/routes/OrganizationRoute.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { CognitoAuthMiddleware } from "../middlewares/AuthMiddleware";
+import EndPointAccessVerifyMiddleware from "../middlewares/EndPointAccessVerifyMiddleware";
+import OrganizationController from "../controllers/OrganizationController";
+
+const organizationRoute = express.Router({ mergeParams: true });
+const organizationController = new OrganizationController();
+
+// routes begining /ielts/organization
+organizationRoute.post("/", CognitoAuthMiddleware, EndPointAccessVerifyMiddleware.SuperAdminAccess, organizationController.createOrganization.bind(organizationController));
+organizationRoute.delete("/:orgId", CognitoAuthMiddleware, EndPointAccessVerifyMiddleware.SuperAdminAccess, organizationController.deleteOrganization.bind(organizationController));
+
+export default organizationRoute;
--- src/routes/UserRoute.ts
+++ src/routes/UserRoute.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import UserController from "../controllers/UserController";
 import { CognitoAuthMiddleware } from "../middlewares/AuthMiddleware";
+import EndPointAccessVerifyMiddleware from "../middlewares/EndPointAccessVerifyMiddleware";
 
 const userRoute = express.Router({ mergeParams: true });
 const userController: UserController = new UserController();
 
 // routes begining /ielts/users
 userRoute.get("/", CognitoAuthMiddleware, userController.getUser.bind(userController));
+userRoute.post("/", CognitoAuthMiddleware, EndPointAccessVerifyMiddleware.allAdminAccess, userController.createUser.bind(userController));
+userRoute.get("/students", CognitoAuthMiddleware, EndPointAccessVerifyMiddleware.allAdminAccess, userController.getStudents.bind(userController));
+userRoute.get("/teachers", CognitoAuthMiddleware, EndPointAccessVerifyMiddleware.allAdminAccess, userController.getTeachers.bind(userController));
 
 export default userRoute;
