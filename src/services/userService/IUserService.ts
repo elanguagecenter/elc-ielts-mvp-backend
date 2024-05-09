3e01f2b667efcb66f0663d37a7fa5433a2779f20
Jayamal <jayamaljayamaha2@gmail.com>
2024-05-09 22:38:10 +0530

    fix: small fixes and delete method in user service

--- src/services/userService/IUserService.ts
+++ src/services/userService/IUserService.ts
@@ -3,6 +3,7 @@ import { CreateUserPayload, UserReponse } from "../../utils/types/common/types";
 interface IUserService {
   getUser(userTypeInToken: string, userId: string): Promise<UserReponse>;
   createUser(userTypeInToken: string, userType: string, userId: string, payload: CreateUserPayload): Promise<UserReponse>;
+  deleteUser(userTypeInToken: string, userId: string, adminUserId: string, userType: string): Promise<UserReponse>;
   getAllAdmins(page: string, limit: string): Promise<Array<UserReponse>>;
   getFreshAdmins(): Promise<Array<UserReponse>>;
   getStudents(userTypeInToken: string, orgId: string, userId: string, page: string, limit: string): Promise<Array<UserReponse>>;
