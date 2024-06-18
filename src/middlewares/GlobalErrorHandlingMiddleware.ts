36f6f5b7f397842e2f24fa84916bba10bee2a46d
Jayamal <jayamal.jayamaha.x@gmail.com>
2024-06-18 23:04:11 +0530

    small fix

--- src/middlewares/GlobalErrorHandlingMiddleware.ts
+++ src/middlewares/GlobalErrorHandlingMiddleware.ts
@@ -2,8 +2,8 @@ import { Response, NextFunction, Request } from "express";
 import ELCIELTSInternalError from "../exception/ELCIELTSInternalError";
 
 export const SimpleErrorHandler = (error: ELCIELTSInternalError, req: Request, res: Response, next: NextFunction) => {
-  res.status(error.getStatus()).send({
-    status: error.getStatus(),
+  res.status(500).send({
+    status: 500,
     message: error.message,
   });
 };
